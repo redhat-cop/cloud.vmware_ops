ec3dffa7949402e22e6a7f94604ffb1809974185
smiron <smiron@redhat.com>
2024-05-20 18:47:44 +0300

    add first integration CI test to run on eco vcenter

--- tests/integration/targets/init.sh
+++ tests/integration/targets/init.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2155,SC2086
 
-BASE_DIR=$(dirname "${BASH_SOURCE[0]}")
+# Export the collections path
+export ANSIBLE_COLLECTIONS_PATH="$ANSIBLE_COLLECTIONS_PATH/ansible_collections"
+
+echo "ANSIBLE_COLLECTIONS_PATH: $ANSIBLE_COLLECTIONS_PATH"
+BASE_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 export ANSIBLE_ROLES_PATH=${BASE_DIR}
