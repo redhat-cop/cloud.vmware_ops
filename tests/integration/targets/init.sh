35c9495be777d5af0cacab9afa42fd2e94bd9965
smiron <smiron@redhat.com>
2024-06-05 19:38:05 +0300

    modify provision_vm_test to fit eco CI

--- tests/integration/targets/init.sh
+++ tests/integration/targets/init.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2155,SC2086
 
-# Export the collections path
-export ANSIBLE_COLLECTIONS_PATH="$ANSIBLE_COLLECTIONS_PATH/ansible_collections"
+export DEFAULT_COLLECTIONS_PATH="$ANSIBLE_COLLECTIONS_PATH/ansible_collections"
+
+# Check if the variable is already set (e.g., in CI)
+if [ -z "$ANSIBLE_COLLECTIONS_PATH" ]; then
+    # If not, use base collections path
+    ANSIBLE_COLLECTIONS_PATH="$DEFAULT_COLLECTIONS_PATH"
+fi
 
 echo "ANSIBLE_COLLECTIONS_PATH: $ANSIBLE_COLLECTIONS_PATH"
 BASE_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
